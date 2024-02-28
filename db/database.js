c5961a7cc42aaa644e361808911ea9d88ad0e015
Emanuel Vidal <emanuelvidalrm@gmail.com>
2024-02-28 00:03:18 -0300

    :sparkles: connect to database

--- db/database.js
+++ db/database.js
@@ -1,21 +1,26 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  port: 3306,
-  dialect: 'mysql'
-});
-
-(async function () {
-  try {
-    await sequelize.authenticate();
-    console.log("Conectado ao banco");
-    await sequelize.sync();
-  } catch (error) {
-    console.error("Unable to connect to the database: ", error);
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.ROOT_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT,
   }
-})();
+)
 
-module.exports = sequelize;
+;(async function () {
+  try {
+    await sequelize.authenticate()
+    console.log('Connection has been established successfully.')
+    await sequelize.sync()
+  } catch (error) {
+    console.error('Unable to connect to the database: ', error)
+  }
+})()
+
+module.exports = sequelize
